22de23f2ed3f1c6451e2a1982eedec7edfcf48f6
juniorralali <afrian.junior@ralali.com>
2020-06-15 17:34:09 +0700

    init

---
+++ gatsby-config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  siteMetadata: {
+    title: `Afrian Junior's Book`,
+    name: `Afrian Junior`,
+    siteUrl: `https://afrianjunior.github.io`,
+    description: ``,
+    hero: {
+      heading: `Afrian Junior's Book`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/narative`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/narative`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/narative.co`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/narative/`,
+      },
+      {
+        name: `dribbble`,
+        url: `https://dribbble.com/narativestudio`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          // contentful: true,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Novela by Narative`,
+        short_name: `Novela`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+      },
+    },
+  ],
+};
